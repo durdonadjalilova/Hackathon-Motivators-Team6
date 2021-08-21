d922544e173f52364a5a901375618b51ef616378
Durdona Djalilova <ddjalilova3@gmail.com>
2021-08-21 11:50:22 -0400

    pages & components setup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import axios from "axios"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/motivators">
+            <Index />
+          </Route>
+          <Route path="*">
+            <FourOFour />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Greeting from '../Components/Greeting'
+import Affirmations from '../Components/Affirmations'
+import Weather from '../Components/Weather'
+import Quotes from '../Components/Quotes'
+
+const Index = () => {
+
+    return(
+        <div>
+            <Greeting />
+            <Weather />
+            <Affirmations />
+            <Quotes />
+        </div>
+    )
+}
+
+export default Index
