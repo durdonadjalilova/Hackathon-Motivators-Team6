da59bf5e68578f10db92276bfe0261ef3f20ad9e
Durdona Djalilova <ddjalilova3@gmail.com>
2021-08-22 13:15:01 -0400

    fix affirmation bug

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,24 +1,19 @@
-
 import React from "react";
-import {useState} from 'react'
+import { useState } from "react";
 import Greeting from "../Components/Greeting";
 import Affirmations from "../Components/Affirmations";
 import Weather from "../Components/Weather/Weather";
 
+const Main = ({ firstName, setFirstName, zipCode }) => {
+  const [keyword, setKeyword] = useState("");
 
+  console.log(keyword);
 
-const Main = ({firstName, setFirstName, zipCode, setZipCode}) => {
-  const [keyword, setKeyword] = useState("")
-
-  console.log(keyword)
-
- 
   return (
     <div>
-      <Greeting firstName={firstName} setFirstName={setFirstName}  />
-      <Weather zipCode={zipCode} setZipCode={setZipCode} setKeyword={setKeyword}  />
-
-   <Affirmations />
+      <Greeting firstName={firstName} setFirstName={setFirstName} />
+      <Weather zipCode={zipCode} setKeyword={setKeyword} />
+      <Affirmations keyword={keyword} />
     </div>
   );
 };
