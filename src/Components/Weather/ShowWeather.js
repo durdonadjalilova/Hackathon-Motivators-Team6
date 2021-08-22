e7180d0a636c421b1711bb03eb479d3f0c387527
Obaid Rustemi <orustemi@gmail.com>
2021-08-22 03:30:08 -0400

    add weather details to display

--- src/Components/Weather/ShowWeather.js
+++ src/Components/Weather/ShowWeather.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function ShowWeather({weatherData}) {
+
+let imageLink = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
     return (
         <div>
-            
+            <h3>{weatherData.name} </h3>
+            <h3>{weatherData.weather[0].main}</h3>
+            <img src={imageLink} placeholder="weather-icon"/>
+            <h3>{Math.round(weatherData.main.temp)}{`° F`}</h3>
         </div>
     )
 }
--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -3,30 +3,32 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import ShowWeather from "./ShowWeather";
 
-const Weather = ({changeKeyword}) => {
+const Weather = ({ changeKeyword }) => {
   const [weatherData, setWeatherData] = useState(null);
+
   const zipCode = 94539;
   const API_KEY = `3be4aceb4cdf59db0f6328334127e108`;
   const API_URL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${API_KEY}`;
 
-  useEffect(() => {
-    const fetchForecastData = async () => {
+  useEffect(async () => {
+    const fetchWeatherData = async () => {
       try {
         const res = await axios.get(`${API_URL}`);
-        changeKeyword(res.data.weather[0].main)
-        setWeatherData(res.data)
-        console.log(res.data)
+
+        changeKeyword(res.data.weather[0].main);
+        setWeatherData(res.data);
+        console.log(res);
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchForecastData();
+    await fetchWeatherData();
   }, []);
   return (
     <div>
       <h3>Here is your weather for today:</h3>
-      <ShowWeather weatherData={weatherData}/>
+      {weatherData && <ShowWeather weatherData={weatherData} />}
     </div>
   );
 };
