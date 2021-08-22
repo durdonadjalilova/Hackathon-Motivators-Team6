cfc1f2084297f5be6dfd8e480da9f738b468238a
Demi <oluwademiladejobi@pursuit.org>
2021-08-22 13:23:37 -0400

    cleaned up comments and logs

--- src/Components/Affirmation.js
+++ src/Components/Affirmation.js
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 const Affirmation = ({ affirmations, keyword }) => {
-//   const [affirmation, setAffirmation] = useState("");
-
-//   let weatherType = "clear";
-
-//   useEffect(() => {
-      
-    const fetchAffirmation = () => {
-     if(keyword.toLowerCase() !== "clear") {
-         return ( affirmations.bad[Math.floor(Math.random() * affirmations.bad.length)]?.phrase)
-     } else {
-         return (affirmations.sun[Math.floor(Math.random() * affirmations.sun.length)]?.phrase)
-     }
-    };
-//     fetchAffirmation();
-//   }, [affirmation, keyword]);
+  const fetchAffirmation = () => {
+    if (keyword.toLowerCase() !== "clear") {
+      return affirmations.bad[
+        Math.floor(Math.random() * affirmations.bad.length)
+      ]?.phrase;
+    } else {
+      return affirmations.sun[
+        Math.floor(Math.random() * affirmations.sun.length)
+      ]?.phrase;
+    }
+  };
 
   return <div> {fetchAffirmation()?.toUpperCase()}</div>;
 };
-
 
 export default Affirmation;
